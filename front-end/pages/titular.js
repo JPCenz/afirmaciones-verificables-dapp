bdaa8247e851ea8ef7ffc286025699d9bcbeab14
Giovanni Luis Barrantes Lazo <gb_12_8@hotmail.com>
2023-07-19 00:17:51 -0500

    #added white color, logo, href to front

--- front-end/pages/titular.js
+++ front-end/pages/titular.js
@@ -255,7 +255,7 @@ export default function TestVerificador() {
                   </a>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">USDC {i.obj.value_amount}</p>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> ID: {i.id}</p>
-                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Banco: {i.obj.banco}</p>
+                  {/* <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"> Banco: {i.obj.banco}</p> */}
                   {/* <a onClick={() => burn(i.id)} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Burn
                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
